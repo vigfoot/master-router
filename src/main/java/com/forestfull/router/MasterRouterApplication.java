b9933218a9a3e7ddf92b257f2305892b335aeb0b
Xasquatch <bigfoot7774@gmail.com>
2024-05-04 16:18:03 +0900

    framework default setting

---
+++ src/main/java/com/forestfull/router/MasterRouterApplication.java
@@ -0,0 +1,13 @@
+package com.forestfull.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterRouterApplication.class, args);
+    }
+
+}
