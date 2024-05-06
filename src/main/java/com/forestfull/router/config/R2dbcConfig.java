1b2554df41ec30d8568e3cdfb2c20dc9d668f515
Xasquatch <bigfoot7774@gmail.com>
2024-05-06 16:39:40 +0900

    커넥션 기본

--- src/main/java/com/forestfull/router/config/R2dbcConfig.java
+++ src/main/java/com/forestfull/router/config/R2dbcConfig.java
@@ -1,9 +1,22 @@
 package com.forestfull.router.config;
 
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 
 @Configuration
-public class R2dbcConfig {
+public class R2dbcConfig extends AbstractR2dbcConfiguration {
 
 
+    @Value("${datasource.url}")
+    private String connectionUrl;
+
+    @Bean
+    @Override
+    public ConnectionFactory connectionFactory() {
+        return ConnectionFactories.get(connectionUrl);
+    }
 }
