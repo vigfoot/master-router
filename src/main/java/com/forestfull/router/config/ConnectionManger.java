a74085394eee4aa9b5bd75348b017ee97771d088
HyeonSeok Ko <bigfoot7774@gmail.com>
2024-05-07 10:20:55 +0900

    리플렉션으로 URI따오기

--- src/main/java/com/forestfull/router/config/ConnectionManger.java
+++ src/main/java/com/forestfull/router/config/ConnectionManger.java
@@ -1,6 +1,7 @@
 package com.forestfull.router.config;
 
 import com.forestfull.router.PostRouter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,6 +10,10 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Configuration
 @EnableWebFluxSecurity
 public class ConnectionManger {
@@ -17,11 +22,23 @@ public class ConnectionManger {
     SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
         return http
                 .authorizeExchange(spec -> {
-                    spec.pathMatchers(HttpMethod.POST, PostRouter.URI.support)
+                    final String[] array = Arrays.stream(PostRouter.URI.class.getFields())
+                            .map(field -> {
+                                try {
+                                    return field.get(PostRouter.URI.class.getFields());
+                                } catch (IllegalAccessException e) {
+                                    e.printStackTrace(System.out);
+                                    log.error(e.getMessage());
+                                    return null;
+                                }
+                            })
+                            .map(String::valueOf)
+                            .toArray(String[]::new);
+                    spec.pathMatchers(HttpMethod.POST, array
+                            )
                             .authenticated();
                 })
                 .build();
-
     }
 
     @Bean
