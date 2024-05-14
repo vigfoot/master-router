0a3cbf5994459a626f4f69affd52c1de270fc6d2
HyeonSeok Ko <bigfoot7774@gmail.com>
2024-05-14 10:08:50 +0900

    jasypt 적용 나머지 알고리즘 및 프러퍼티 설정은 전부 default값으로 적용

---
+++ src/main/java/com/forestfull/router/config/UtilsConfig.java
@@ -0,0 +1,38 @@
+package com.forestfull.router.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+
+@Configuration
+public class UtilsConfig {
+
+    @Value("${spring.profiles.active}")
+    private String key;
+
+    @Bean("jasyptStringEncryptor")
+    StringEncryptor stringEncryptor() {
+        return EncryptionSetting.initEncryptor(key);
+    }
+
+    private static class EncryptionSetting {
+        public static void main(String[] args) {
+
+        }
+        private static final String privateKey = null;
+        private static StringEncryptor initEncryptor(String key) {
+            final PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+            encryptor.setPassword(key);
+            encryptor.setKeyObtentionIterations(1000);
+            encryptor.setPoolSize(1);
+            return encryptor;
+        }
+
+        private static final Consumer<String> enc = str
+                -> System.out.println(str + " : ENC(" + initEncryptor(privateKey).encrypt(str) + ")");
+    }
+}
