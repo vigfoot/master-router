e94e1a4c7da411eb3ed9433a9d8d4c0a56284857
HyeonSeok Ko <bigfoot7774@gmail.com>
2024-05-21 12:47:46 +0900

    401 핸들링 추가

--- src/main/java/com/forestfull/router/config/ExceptionResponse.java
+++ src/main/java/com/forestfull/router/config/ExceptionResponse.java
@@ -3,6 +3,7 @@ package com.forestfull.router.config;
 import com.forestfull.router.entity.NetworkVO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,10 +16,13 @@ public class ExceptionResponse {
     public static NetworkVO.Response<String> isError(Exception e) {
         e.printStackTrace(System.out);
 
-        if (Objects.equals(HttpStatus.UNAUTHORIZED.name(), e.getMessage()))
+        if ("Access Denied".equalsIgnoreCase(e.getMessage()))
             return NetworkVO.Response.fail(HttpStatus.UNAUTHORIZED, NetworkVO.DATA_TYPE.ERROR.name());
 
-        if (Objects.equals(HttpStatus.BAD_REQUEST.name(), e.getMessage()))
+        if (HttpStatus.UNAUTHORIZED.name().equalsIgnoreCase(e.getMessage()))
+            return NetworkVO.Response.fail(HttpStatus.UNAUTHORIZED, NetworkVO.DATA_TYPE.ERROR.name());
+
+        if (HttpStatus.BAD_REQUEST.name().equalsIgnoreCase(e.getMessage()))
             return NetworkVO.Response.fail(HttpStatus.BAD_REQUEST, NetworkVO.DATA_TYPE.ERROR.name());
 
         return NetworkVO.Response.fail(HttpStatus.INTERNAL_SERVER_ERROR, NetworkVO.DATA_TYPE.ERROR.name());
